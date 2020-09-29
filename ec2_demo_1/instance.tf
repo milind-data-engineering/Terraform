9ff874752346e330ced5af8c16f80a7ff4784f97
Milind <milind270989@gmail.com>
2020-09-29 08:20:24 -0400

    First EC2 terraform instance

---
+++ ec2_demo_1/instance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+	access_key = "AKIAWM6ONOTEMRBJUVP6"
+	secret_key = "GsYOfLK12fVZ9LG7g/gLs4HCkLMqmYUFjFPYi2ND"
+	region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-00514a528eadbc95b"
+	instance_type = "t2.micro"
+}
