7a3fe93061ad1db5206ed337b7150ebb380b7f83
Milind <milind270989@gmail.com>
2020-09-29 22:08:47 -0400

    Changed region

--- instance.tf
+++ instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
 	access_key = "AKIAWM6ONOTEMRBJUVP6"
 	secret_key = "GsYOfLK12fVZ9LG7g/gLs4HCkLMqmYUFjFPYi2ND"
-	region = "us-west-1"
+	region = "us-west-2"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-088c153f74339f34c"
+    ami = "ami-0528a5175983e7f28"
 	instance_type = "t2.micro"
 }
